d58b76c8f7e41cb9921037d1269938f64061fd1f
Djiney <DjineyX@gmail.com>
2020-04-03 17:04:05 +0300

    HomeWork: Substring, better solution

--- src/telran/additional/_08_Strings/Main.java
+++ src/telran/additional/_08_Strings/Main.java
@@ -11,44 +11,30 @@ public class Main
 	 * 2. No cycles;
 	 * 3. No static fields
 	 */
-
 	public static boolean isSubstring(String string, String substring)
+	{
+		return find(string, substring, 0);
+	}
+
+	private static boolean find(String string, String substring, int index)
 	{
 		if (!isDataValid(string, substring)) {
 			return false;
 		}
 
-		return find(string, substring, 0);
+		if (index >= substring.length()) {
+			return true;
+		}
+
+		if (string.charAt(index) == substring.charAt(index)) {
+			return find(string, substring, index + 1);
+		}
+
+		return find(string.substring(1), substring, 0);
 	}
 
 	private static boolean isDataValid(String string, String substring)
 	{
 		return string.length() > 0 && substring.length() > 0 && string.length() >= substring.length();
-	}
-
-	private static boolean find(String string, String substring, int index)
-	{
-		if (index >= string.length()) {
-			return false;
-		}
-
-		if (checkSubstring(string, substring, index, 0)) {
-			return true;
-		}
-
-		return find(string, substring, index + 1);
-	}
-
-	private static boolean checkSubstring(String string, String substring, int stringIndex, int substringIndex)
-	{
-		if (substringIndex >= substring.length()) {
-			return true;
-		}
-
-		if (string.charAt(stringIndex) != substring.charAt(substringIndex)) {
-			return false;
-		}
-
-		return checkSubstring(string, substring, stringIndex + 1, substringIndex + 1);
 	}
 }
